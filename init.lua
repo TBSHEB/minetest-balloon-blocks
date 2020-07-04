1909c304466be12a83d4645065b4a95dd4f16edf
TBSHEB <TBSHEB@carthew.net>
2020-07-04 10:06:24 +1000

    Refactored common.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ balloonblocks = {}
 
 local modpath = minetest.get_modpath("balloonblocks")
 
-dofile(modpath.."/common.lua")
+dofile(modpath.."/src/common.lua")
 dofile(modpath.."/src/regular.lua")
 dofile(modpath.."/src/glowing.lua")

3e1bc604407b8a34aaa1291d80b222719111a176
Evgeniy <evgenuel@gmail.com>
2025-04-17 07:29:02 +0300

    code cleaning,fix disabling stairs if no moreblocks present

--- init.lua
+++ init.lua
@@ -7,5 +7,9 @@ local modpath = minetest.get_modpath("balloonblocks")
 dofile(modpath.."/src/common.lua")
 dofile(modpath.."/src/regular.lua")
 dofile(modpath.."/src/glowing.lua")
-dofile(modpath.."/src/stairs.lua")
+
+if minetest.global_exists("stairsplus") then
+	dofile(modpath.."/src/stairs.lua")
+end
+
 minetest.log("action", "[Mod] "..mname.." ["..version.."] loaded.")
--- src/stairs.lua
+++ src/stairs.lua
@@ -1,33 +1,3 @@
---if minetest.global_exists("stairsplus") then
---образец ноды, которую надо обработать
---[[
-minetest.register_node("balloonblocks:glowing_red", {
-  description = "Glowing red balloon",
-  tiles = {"balloonblocks_red.png"},
-	groups = balloonblocks.groups,
-	light_source = default.LIGHT_MAX,
-	paramtype = "light",
-	sunlight_propagates = true,
-	on_secondary_use = balloonblocks.placeGlowRed,
-	sounds = balloonblocks.sounds
-})
---образец кода добавления в пилу ноды
-stairsplus:register_all("df_underworld_items", "glow_amethyst", "df_underworld_items:glow_amethyst", {
-	description = S("Glowing Amethyst Block"),
-	tiles = {"dfcaverns_glow_amethyst.png"},
-	groups = {crumbly = 1, cracky = 3},
-	sounds = df_dependencies.sound_glass(),
-	is_ground_content = false,
-	light_source = 6,
-	paramtype = "light",
-	use_texture_alpha = "blend",
-	drawtype = "glasslike",
-	sunlight_propagates = true,
-})
-
---]]
---код итерирующий все зарегистрированные ноды мода, и цепляющий ни нах регистрацию в стайрс
-
 local names = {}
 local node_def = {}
 local nodes = {}
